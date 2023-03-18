1aad3b9e374366c2df607a577240ddd8bfde1a1a
Elielso Francisco <107587443+Elielsouk@users.noreply.github.com>
2023-03-18 18:35:09 +0000

    Assignment Submission # 8
    Basic  Javascrpit projects. Thanks

---
+++ main.js
@@ -0,0 +1,16 @@
+let text1 = "Hello";
+let text2 = "world!";
+let result = text1.concat(" ", text2);
+
+document.getElementById("demo").innerHTML = result;
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.getElementById("demo1").innerHTML = text.length;
+
+let text = "Hello World!";
+let result = text;
+
+document.getElementById("demo3").innerHTML = result; 
+
+let num = 13.3714;
+document.getElementById("demo4").innerHTML = num.toPrecision(2);
